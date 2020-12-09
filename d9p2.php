c2bd7b64d580861f0405f027da2c07dbd3ee8e2a
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-12-09 09:08:03 +0000

    Day 9

---
+++ d9p2.php
@@ -0,0 +1,80 @@
+<?php
+
+$code = rtrim(file_get_contents('d9.txt'));
+// $code = "35
+// 20
+// 15
+// 25
+// 47
+// 40
+// 62
+// 55
+// 65
+// 95
+// 102
+// 117
+// 150
+// 182
+// 127
+// 219
+// 299
+// 277
+// 309
+// 576";
+$code = explode("\n", $code);
+
+$preamble = 25;
+
+$premableArray = array_slice($code, 0, $preamble);
+$remainingArray = array_slice($code, $preamble, count($code));
+
+$preamblePosition = 0;
+foreach ($remainingArray as $nextKey => $nextValue)
+{
+	foreach ($premableArray as $tryKey => $tryValue)
+	{
+		$searchArray = $premableArray;
+		unset($searchArray[$tryKey]);
+		$searchValue = $nextValue - $tryValue;
+
+		$found = array_search($searchValue, $premableArray);
+
+		if ($found !== false)
+		{
+			break;
+		}
+	}
+
+	unset($premableArray[$preamblePosition++]);
+	$premableArray[] = $nextValue;
+
+	if ($found === false)
+	{
+		break;
+	}
+}
+
+$searchValue = $nextValue;
+$searchValues = array_slice($code, 0, $nextKey);
+
+foreach ($searchValues as $currKey => $currValue)
+{
+	$sum = $currValue;
+
+	$sumValues = [$currValue];
+
+	for ($i = $currKey + 1; $i < count($searchValues), $sum < $searchValue; $i++)
+	{
+		$sum += $searchValues[$i];
+		$sumValues[] = $searchValues[$i];
+
+		if ($sum === (int)$searchValue)
+		{
+			break 2;
+		}
+	}
+
+	unset($searchValues[$currKey]);
+}
+
+echo min($sumValues) + max($sumValues), "\n";
